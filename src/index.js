2ba50c696a9989100b512d4c9bed5e4e10e41d8b
TMDNTDL <81274608+TMDNTDL@users.noreply.github.com>
2024-09-20 12:48:17 -0400

    initialize RTK manage data and axios get data

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import sum from '@/test';
 import { RouterProvider } from 'react-router-dom';
 import router from '@/router'
+import { Provider } from 'react-redux';
+// import theme
+import './theme.css'
+import store from './store'
 
 
 const total =  sum(1,3)
@@ -13,8 +17,10 @@ console.log(total)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-   <RouterProvider router = {router}/>
+    <Provider store={store}>
+        <RouterProvider router = {router}/>
+    </Provider>
+   
   
 );
 
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,12 +1,25 @@
 import { Outlet } from "react-router-dom"
+import { Button } from "antd-mobile"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { getBillList } from "@/store/modules/billStore"
 
 const Layout = () =>{
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(getBillList())
+    }, [dispatch])
     return(
         <div>
             {/* after define children router in the router folder and class, we need to define a exit for the children to be render out in the first-class router */}
-            
+
             <Outlet />
             我是layout
+            {/* test global render */}
+            <Button color="primary">TESTING</Button>
+            <div className="purple">
+                <Button color="primary"> partial TESTING</Button>
+            </div>
         </div>
     ) 
 }
