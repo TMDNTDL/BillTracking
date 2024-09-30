46e71034c26613c47758741ee239ea00c250a907
TMDNTDL <81274608+TMDNTDL@users.noreply.github.com>
2024-09-30 15:33:27 -0400

    Initiall structure of the billing system

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,5 +1,48 @@
+import { DatePicker } from 'antd-mobile';
+import './index.scss';
 const Month = () =>{
-    return <div>我是Month</div>
+    return( 
+        <div className="monthyBill">
+            <div className="title">
+                Monthly Bill
+            </div>
+            <div className="header">
+                <div className="date">
+                    <span className="text">2023 | Feb bill</span>
+                    <span className='arrow expand'></span>
+                </div>
+                <div className="summary">
+                    <div className="spent">
+                        <span className="spent-amount">{100}</span>
+                        <div className="title-spent">支出</div>
+                    </div>
+                    <div className="income">
+                        <span className="income-amount">{200}</span>
+                        <div className="title-income">收入</div>
+                    </div>
+                    <div className="remain">
+                        <span className="remain-amount">{100}</span>
+                        <div className="title-remain">结余</div>
+                    </div>
+                </div>
+
+                {/* time picker API*/}
+                <DatePicker
+                    className="kaDate"
+                    title='Track Date'
+                    precision='month'
+                    visible={false}
+                    max={new Date()}
+                />
+            </div>
+
+
+            
+        </div>
+        
+
+    )
+
 }
 
 export default Month
