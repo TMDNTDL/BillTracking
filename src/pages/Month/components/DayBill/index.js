55e1c7b7ad32e9dd24546163f2deb89a180eb1cf
TMDNTDL <81274608+TMDNTDL@users.noreply.github.com>
2024-10-16 01:07:21 -0400

    adding dayBillSummary (dynamic) based on the data

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -1,25 +1,41 @@
 import classNames from "classnames";
 import './index.scss'
-const DayBill = ()=>{
+import { useMemo } from 'react'
+const DayBill = ({date, billList})=>{
+    console.log(date)
+    console.log(billList)
+
+    // calculate the result
+    const dayResult = useMemo(() =>{
+        const pay = billList.filter(item => item.type === 'pay').reduce((a,c) => a + c.money, 0)
+        const income = billList.filter(item => item.type ==='income').reduce((a,c) => a + c.money, 0)
+        const remain = income + pay
+        return {
+            pay, 
+            income,
+            remain
+        }
+    }, [billList])
+
     return(
         <div className="dayBillContainer">
             <div className="bills">
                 <div className="header">
-                    <span className="date">{"03/25"}</span>
+                    <span className="date">{date}</span>
                     <span className="arrow"></span>
                 </div>
                 <div className="MoneySummary">
                     <div className="spent">
                         <span className="spent-title">spent</span> 
-                        <span className="spent-amount">{100}</span>
+                        <span className="spent-amount">{dayResult.pay.toFixed(2)}</span>
             
                     </div>
                     <div className="income">
                         <span className="income-title">income</span>
-                        <span className="income-amount">{100}</span>
+                        <span className="income-amount">{dayResult.income.toFixed(2)}</span>
                     </div>
                     <div className="Remain">
-                        <span className="remain-amount">{100}</span>
+                        <span className="remain-amount">{dayResult.remain.toFixed(2)}</span>
                         <span className="remain-title">remain</span>
                     </div>
                     
